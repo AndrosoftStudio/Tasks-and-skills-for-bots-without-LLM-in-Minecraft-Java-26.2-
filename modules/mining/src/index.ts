6bb985340cd4c642ae20236ae44226b79ad62b40
Cambrero Corporation <106564630+AndrosoftStudio@users.noreply.github.com>
2026-09-06 15:38:21 -0300

    feat(mining): wire Navigation V1.1 adapter into public API

--- modules/mining/src/index.ts
+++ modules/mining/src/index.ts
@@ -2,7 +2,9 @@ export * from './types.js'
 export * from './ToolSelector.js'
 export * from './MiningService.js'
 export * from './MineflayerMiningWorld.js'
+export * from './NavigationV11Adapter.js'
 import { MiningService } from './MiningService.js'
+import { adaptNavigationV11, type NavigationV11Like } from './NavigationV11Adapter.js'
 import type { CollectDropParams, FindBlockParams, MineBlockParams, MineNearestParams, MineVeinParams, MiningNavigation, MiningResult, MiningRunOptions, MiningWorld, MiningBlock, MinedBlockData, MineVeinData } from './types.js'
 export interface MiningAPI {
   findBlocks(params:FindBlockParams,options?:MiningRunOptions):Promise<MiningResult<MiningBlock[]>>
@@ -15,3 +17,4 @@ export function createMining(options:{world:MiningWorld;navigation:MiningNavigat
   const service=new MiningService(options.world,options.navigation)
   return{findBlocks:(p,o)=>service.findBlocks(p,o),mineBlock:(p,o)=>service.mineBlock(p,o),mineNearest:(p,o)=>service.mineNearest(p,o),collectDrop:(p={},o)=>service.collectDrop(p,o),mineVein:(p,o)=>service.mineVein(p,o)}
 }
+export function createMiningForNavigationV11(options:{world:MiningWorld;navigation:NavigationV11Like}):MiningAPI{return createMining({world:options.world,navigation:adaptNavigationV11(options.navigation)})}
